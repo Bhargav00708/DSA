88a854094b03e24603325d9382c199de08c19b97
Bhargav0508 <bhargav@tryspeed.com>
2024-07-26 19:03:14 +0530

    Add desc for detect cycle.

--- Striver A2Z DSA Sheet/SLL/detectCycle.ts
+++ Striver A2Z DSA Sheet/SLL/detectCycle.ts
@@ -1,7 +1,13 @@
+// see strivers video for batter understanding
+// here what happen that if fast and slow will meet according to floydes algorithms so
+// here if we move any pointer fast or slow to head after meating and move both one point ahead so eventually
+// we find starting point here head to loop start distance is L1 of slow so fast distance is L1 + L1 and distance between
+// slow and fast is D so length of the loop is L1(distance between slow and fast is L1) + D so that's why we are getting that.
+
 var detectCycle = function (head) {
   let fast = head;
   let slow = head;
-  while (fast && fast.next) {
+  while (fast?.next) {
     slow = slow.next;
     fast = fast.next.next;
     if (fast === slow) {
@@ -15,4 +21,3 @@ var detectCycle = function (head) {
   }
   return null;
 };
-
